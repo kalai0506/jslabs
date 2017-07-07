3179947ad5c6179ae5261250976804c9a8f85257
kalai <eie.kalai@gmail.com>
2017-07-07 00:10:00 -0400

    Understanding nodeJS Modules

---
+++ sample1/lab1.js
@@ -0,0 +1,7 @@
+var math=require('./math.js');
+
+console.log(math.add(1,2));
+
+console.log(math.subtract(1,2));
+
+console.log(math.multiply(1,2));
---
+++ sample1/math.js
@@ -0,0 +1,14 @@
+exports.add = function (num1, num2){
+  console.log('Inside Addition');
+  return num1+num2;
+};
+
+exports.subtract = function (num1, num2){
+  console.log('Inside subtract');
+  return num1-num2;
+};
+
+exports.multiply = function (num1, num2){
+  console.log('Inside multiply');
+  return num1*num2;
+};
