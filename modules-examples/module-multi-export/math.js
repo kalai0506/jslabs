c2bead7337252a24cdfb29dca8d483aca0335e9b
kalai <eie.kalai@gmail.com>
2017-07-07 00:52:02 -0400

    Added multi module export example

---
+++ modules-examples/module-multi-export/math.js
@@ -0,0 +1,16 @@
+add = function (num1, num2){
+  console.log('Inside Addition');
+  return num1+num2;
+};
+
+subtract = function (num1, num2){
+  console.log('Inside subtract');
+  return num1-num2;
+};
+
+multiply = function (num1, num2){
+  console.log('Inside multiply');
+  return num1*num2;
+};
+
+module.exports={add,subtract};
